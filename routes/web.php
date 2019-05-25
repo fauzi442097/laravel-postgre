0ad2b783c0b04ae1ffd6262a3b610680a9209595
fauzi442097 <fauzi442097@gmail.com>
2019-05-25 11:41:27 +0700

    Menambah route untuk kelola data mata kuliah

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,25 @@
 
 //VIEW
 Route::get('/', 'IndexController@index');
+
+//Mahasiswa
 Route::get('/mahasiswa', 'MahasiswaController@index');
-Route::get('/matakuliah', 'MataKuliahController@index');
-
 Route::get('/mahasiswa/tambah', 'MahasiswaController@TambahData');
-Route::get('/mahasiswa/ubah/{nim}', 'MahasiswaController@UbahData');
 
-//CRUD
+
+//MataKuliah
+Route::get('/matakuliah', 'MataKuliahController@index');
+Route::get('/matakuliah/tambah', 'MataKuliahController@TambahData');
+Route::get('/matakuliah/ubah/{kodemk}', 'MataKuliahController@UbahData');
+
+
+
+//crud
+//Mahasiswa
 Route::post('/mahasiswa/tambah', 'MahasiswaController@TambahData');
 Route::put('/mahasiswa/ubah/{nim}', 'MahasiswaController@UbahData');
 Route::get('/mahasiswa/hapus/{nim}', 'MahasiswaController@DeleteData');
+
+//MataKuliah
+Route::post('/matakuliah/tambah', 'MataKuliahController@TambahData');
+Route::put('/matakuliah/ubah/{kodemk}', 'MataKuliahController@UbahData');
